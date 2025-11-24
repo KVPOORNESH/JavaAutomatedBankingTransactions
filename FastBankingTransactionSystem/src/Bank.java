7ed0598b1bef3b0ad16c3fbca29c4355d959f863
KVPOORNESH <kvpoornesh2019@gmail.com>
2025-11-25 01:43:28 +0530

    Update Bank.java

--- FastBankingTransactionSystem/src/Bank.java
+++ FastBankingTransactionSystem/src/Bank.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
+
+public class Bank {
+    ArrayList<Account> accounts = new ArrayList<>();
+
+    public void addAccount(Account acc) {
+        accounts.add(acc);
+    }
+
+    public Account findAccount(String accNum) {
+        for (Account acc : accounts) {
+            if (acc.accountNumber.equals(accNum)) return acc;
+        }
+        return null;
+    }
+}
 
