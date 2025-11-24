e5d6af1f349b11304f3b93021720c0baaf995968
KVPOORNESH <kvpoornesh2019@gmail.com>
2025-11-25 01:41:08 +0530

    Update Account.java

--- FastBankingTransactionSystem/src/Account.java
+++ FastBankingTransactionSystem/src/Account.java
@@ -1,1 +1,16 @@
+public class Account {
+    String accountNumber;
+    String accountName;
+    int balance;
+
+    public Account(String number, String name, int bal) {
+        accountNumber = number;
+        accountName = name;
+        balance = bal;
+    }
+
+    public void showAccount() {
+        System.out.println("Account: " + accountNumber + ", Name: " + accountName + ", Balance: " + balance);
+    }
+}
 
